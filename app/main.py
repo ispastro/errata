9e358e313f9641a24cd58e6c9fd9d06db7e7ee55
Haile Asaye <haileasaye51@gmail.com>
2025-12-13 22:02:08 +0300

    health route done

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 from fastapi  import FastAPI
+from app.api.v1.health import router as health_router
+
+app = FastAPI(title="Doct Tutur AI Backend API")
 
 
-app = FastAPI()
-
-@app.get('/health')
-async  def health_check():
-    return {"status":"ok"}
+app.include_router(health_router)
