015cf520570e80697a82c7964fe4f6463c33cf36
Haile Asaye <haileasaye51@gmail.com>
2026-01-17 23:44:51 +0300

    feat: database engine created

---
+++ app/core/database.py
@@ -0,0 +1,17 @@
+from sqlmodel import  create_engine, Session, SQLModel
+from app.core.config import  settings
+
+
+
+engine = create_engine(
+      settings.database_url,
+      echo=settings.database_echo
+)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session   
